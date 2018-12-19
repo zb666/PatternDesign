88f4f1bdf8e223e5f8bef8b891cbb78830faf992
zb666 <1360799362@qq.com>
2018-12-19 11:27:24 +0800

    Initial commit

---
+++ src/PatternDesign.java
@@ -0,0 +1,15 @@
+public class PatternDesign {
+
+    public static void main(String args[]){
+        String start = "a";
+        System.out.print("运行");
+
+    }
+
+    public void matchStr(String str){
+        if ("c".equals(str)){
+            
+        }
+    }
+
+}
