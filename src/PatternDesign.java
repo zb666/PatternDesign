e9d23d54485c6c9570c8182aa58034468663e146
zb666 <1360799362@qq.com>
2018-12-19 11:29:45 +0800

    工厂

--- src/PatternDesign.java
+++ src/PatternDesign.java
@@ -3,12 +3,16 @@ public class PatternDesign {
     public static void main(String args[]){
         String start = "a";
         System.out.print("运行");
-
+        matchStr(start);
     }
 
-    public void matchStr(String str){
+    public static void matchStr(String str){
         if ("c".equals(str)){
-            
+            System.out.print("ccc");
+        }else if ("d".equals(str)){
+            System.out.print("ddd");
+        }else if ("a".equals(str)){
+            System.out.print("aaa");
         }
     }
 
